9ddcadf38511f4d35077f6d26b9e653d3818c806
Randy Nava <randyn1080@gmail.com>
2019-12-21 14:18:55 -0500

    combined /parseraid addon to follow /snap addon logic

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
-SLASH_SGC = "/snap"
+RaidMembers = {}
+
+SLASH_SGC1 = "/snap"
 
 StaticPopupDialogs["CopyDialog"] = {
     text = "Press Ctrl-C to copy the text.",
@@ -9,24 +11,42 @@ StaticPopupDialogs["CopyDialog"] = {
     preferredIndex = 3,
     hasEditBox = true,
     maxLetters = 1000,
-    OnShow = function(self)
+    OnAccept = function() -- add another edit box with an input option to pass through boss / DKP gain?
+    
+        tinsert(RaidMembers,"====================")
+        for i=1, GetNumGroupMembers() do
+            local name, rank, subgroup, level, class, fileName, zone, online = GetRaidRosterInfo(i)
+            if online then
+                tinsert(RaidMembers,format("%s, %s",date(),name))
+            end
+        end
+        tinsert(RaidMembers,"====================")
+    
+        print('completed SavedVar upload')
+    
     end
 }
 
 SlashCmdList.SGC = function(input)
+
+    if (not IsInGroup()) then
+        print("you aren't in a group")
+        return
+    end
     
     local playerName = ""
 
-    for i = 0, 40 do
-        name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML, combatRole = GetRaidRosterInfo(i)
+    for i = 1, GetNumGroupMembers() do
+        local name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML, combatRole = GetRaidRosterInfo(i)
         playerName = playerName .. name .. ";"
     end
 
 
-    local dialog = StaticPopup_show("CopyDialog")
+    local dialog = StaticPopup_Show("CopyDialog")
     local editBox = dialog.editBox
 
     editBox:SetText(playerName)
     editBox:HighlightText()
     editBox:SetFocus()
+
 end
