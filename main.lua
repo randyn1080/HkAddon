d90ed30559cfd426f229a37a7ed159398d2a47fa
Randy Nava <randyn1080@gmail.com>
2019-12-31 15:09:00 -0500

    changed some logic, saving to saved var now through 2 seperate /cmds: /record & :purchase.

--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
 RaidMembers = {}
+LootHistory = {}
 
 SLASH_SGC1 = "/clutch"
+SLASH_RECORD1 = "/record"
+SLASH_PURCHASE1 = "/purchase"
 
 StaticPopupDialogs["CopyDialog"] = {
     text = "Press Ctrl-C to copy the text.",
@@ -11,22 +14,22 @@ StaticPopupDialogs["CopyDialog"] = {
     preferredIndex = 3,
     hasEditBox = true,
     maxLetters = 1000,
-    OnAccept = function() -- add another edit box with an input option to pass through boss / DKP gain?
+    -- OnAccept = function() -- add another edit box with an input option to pass through boss / DKP gain?
     
-        tinsert(RaidMembers,"====================")
-        for i=1, GetNumGroupMembers() do
-            local name, rank, subgroup, level, class, fileName, zone, online = GetRaidRosterInfo(i)
-            if online then
-                if UnitIsInMyGuild(format("%s",name)) then                    
-                    tinsert(RaidMembers,format("%s, %s",date(),name))
-                end
-            end
-        end
-        tinsert(RaidMembers,"====================")
+    --     tinsert(RaidMembers,"====================")
+    --     for i=1, GetNumGroupMembers() do
+    --         local name, rank, subgroup, level, class, fileName, zone, online = GetRaidRosterInfo(i)
+    --         -- if online then
+    --             if UnitIsInMyGuild(format("%s",name)) then                    
+    --                 tinsert(RaidMembers,format("%s, %s",date(),name))
+    --             end
+    --         -- end
+    --     end
+    --     tinsert(RaidMembers,"====================")
     
-        print('completed SavedVar upload')
+    --     print('completed SavedVar upload')
     
-    end
+    -- end
 }
 
 SlashCmdList.SGC = function(input)
@@ -40,11 +43,11 @@ SlashCmdList.SGC = function(input)
 
     for i = 1, GetNumGroupMembers() do
         local name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML, combatRole = GetRaidRosterInfo(i)
-        if online then
+        -- if online then
             if UnitIsInMyGuild(format("%s",name)) then
                 playerName = playerName .. name .. ";"
             end
-        end                
+        -- end                
     end
 
 
@@ -54,5 +57,30 @@ SlashCmdList.SGC = function(input)
     editBox:SetText(playerName)
     editBox:HighlightText()
     editBox:SetFocus()
+
+end
+
+SlashCmdList.RECORD = function(msg)
+
+    tinsert(RaidMembers,"====================")
+        for i=1, GetNumGroupMembers() do
+            local name, rank, subgroup, level, class, fileName, zone, online = GetRaidRosterInfo(i)
+            if online then
+                if UnitIsInMyGuild(format("%s",name)) then                    
+                    tinsert(RaidMembers,format("%s, %s, %s",date(),name,msg))
+                end
+            end
+        end
+    tinsert(RaidMembers,"====================")
+
+    print('Completed Recording DKP')
+
+end
+
+SlashCmdList.PURCHASE = function(msg)
+
+    tinsert(LootHistory, format("%s, %s",date(), msg))
+
+    print('Completed Recording Purchase DKP')
 
 end
